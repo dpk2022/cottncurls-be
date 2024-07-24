ac5fd79a7b6aa10d31aa23770a41b9d6377788b9
deepak9013216128 <deepak9013216128@gmail.com>
2024-07-24 16:33:47 +0530

    update database

--- config/database.js
+++ config/database.js
@@ -55,23 +55,16 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"),
+        connectionString: env(
+          "DATABASE_URL",
+          "postgresql://cottncurls:27hLEVJpFBqDtK0XA7ZziV7StrPEV7F0@dpg-cqgdmkiju9rs73cc7b30-a.oregon-postgres.render.com/cottncurls"
+        ),
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "postgres"),
         user: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "postgres"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
+        ssl: env.bool("DATABASE_SSL", true),
         schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
